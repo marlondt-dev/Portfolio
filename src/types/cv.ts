429929ddb1e83299cd3a3379bedf1eb072716916
Marlon Torres <marlon.dt@hotmail.com>
2025-09-08 11:34:38 +0200

    feat(types): created types for cv info

---
+++ src/types/cv.ts
@@ -0,0 +1,56 @@
+export interface CVBasics {
+  name: string;
+  label: string;
+  image: string;
+  email: string;
+  phone: string;
+  url: string;
+  summary: string;
+  location: {
+    city: string;
+    region: string;
+  };
+  profiles: {
+    network: string;
+    username: string;
+    url: string;
+  }[];
+}
+
+export interface Project {
+  name: string;
+  url: string;
+  description: string;
+  highlights: string[];
+  isActive: boolean;
+}
+
+export interface WorkExperience {
+  name: string;
+  position: string;
+  startDate: string;
+  endDate?: string;
+  summary: string;
+  highlights: string[];
+}
+
+export interface Education {
+  institution: string;
+  area: string;
+  studyType: string;
+  startDate: string;
+  endDate?: string;
+}
+
+export interface Skill {
+  name: string;
+  keywords: string[];
+}
+
+export interface CV {
+  basics: CVBasics;
+  work: WorkExperience[];
+  education: Education[];
+  projects: Project[];
+  skills: Skill[];
+}
